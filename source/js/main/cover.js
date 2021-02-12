e8efa18b87457af92d2f3a8aedfa41fe354c084e
katareena <w10160177@gmail.com>
2021-02-12 14:09:42 +0300

    add touch event for cover, clear code

--- source/js/main/cover.js
+++ source/js/main/cover.js
@@ -14,11 +14,11 @@
       document.removeEventListener('mousemove', mouseMoveHandler);
       document.removeEventListener('click', closeOverlayHandler);
     }
-  };
+  }
 
   var limits = {
     right: window.innerWidth - CURSOR_WIDTH,
-    bottom: window.innerHeight - CURSOR_WIDTH
+    bottom: window.innerHeight - CURSOR_WIDTH,
   };
 
   function mouseMoveHandler(evt) {
@@ -38,14 +38,14 @@
         cursor.style.top = limits.bottom + 'px';
       }
     }
-  };
+  }
 
   function resetAllInpuns() {
-    var inputs = document.querySelectorAll('input')
+    var inputs = document.querySelectorAll('input');
     inputs.forEach(function (el) {
       el.value = '';
     });
-  };
+  }
 
   function setPageHandler() {
     mainWrap.classList.add('visually-hidden');
@@ -54,9 +54,67 @@
     window.countdown.getDefaultCountdown();
     resetAllInpuns();
     document.addEventListener('mousemove', mouseMoveHandler);
+    document.addEventListener('touchstart', touchStartHandler, false);
+    document.addEventListener('touchmove', touchMoveHandler, false);
+    document.addEventListener('touchend', touchEndHandler, false);
   }
 
   document.addEventListener('DOMContentLoaded', setPageHandler);
   document.addEventListener('click', closeOverlayHandler);
 
+  // ----------------- touch event -----------------
+  var x = 0;
+  var y = 0;
+
+  console.log(x);
+  console.log(y);
+
+  function touchStartHandler(evt) {
+    if (cover.classList.contains('show')) {
+      switch (evt.touches.length) {
+        case 1:
+          console.log('1 touch');
+          x = evt.touches[0].clientX;
+          y = evt.touches[0].clientY;
+          // evt.preventDefault();
+          break;
+        case 2:
+          console.log('2 touches');
+          evt.preventDefault();
+          break;
+        case 3:
+          console.log('3 touches');
+          evt.preventDefault();
+          break;
+        default:
+          console.log('Not supported');
+          evt.preventDefault();
+          break;
+      }
+    }
+  }
+
+  function touchMoveHandler(evt) {
+    if (cover.classList.contains('show')) {
+      if (evt.touches.length === 1) {
+        console.log(`MovingX... ${Math.abs(x - evt.touches[0].clientX)}px`);
+        console.log(`MovingY... ${Math.abs(y - evt.touches[0].clientY)}px`);
+
+        cursor.style.left = `${Math.abs(x - evt.touches[0].clientX)}px`;
+        cursor.style.top = `${Math.abs(y - evt.touches[0].clientY)}px`;
+      }
+    }
+  }
+
+  function touchEndHandler(evt) {
+    if (cover.classList.contains('show')) {
+      console.log('end move');
+      x = 0;
+      y = 0;
+      cover.classList.remove('show');
+      cursor.classList.remove('show');
+      mainWrap.classList.remove('visually-hidden');
+      evt.preventDefault();
+    }
+  }
 })();
